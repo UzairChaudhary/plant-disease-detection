558789ee9baca7b898dfb4c2f4d3d85fe31b27d3
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-06-02 16:43:50 +0500

    added gunicorn wsgi

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def predict_url_handler(project=None, publishedName=None):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     initialize()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
